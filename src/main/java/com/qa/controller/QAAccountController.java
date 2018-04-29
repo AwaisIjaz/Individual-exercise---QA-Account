e44f6afd647e6a1f8b199522b37b943bf573b2e9
AwaisIjaz <Awais_ijaz786@hotmail.com>
2018-04-29 23:45:11 +0100

    app complete

--- src/main/java/com/qa/controller/QAAccountController.java
+++ src/main/java/com/qa/controller/QAAccountController.java
@@ -2,6 +2,8 @@ package com.qa.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,34 +11,42 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.model.QAAccount;
+import com.qa.repository.QAAccountRepository;
 
 @RestController
 @RequestMapping("api/v1/")
 public class QAAccountController {
 
+	@Autowired
+	private QAAccountRepository qaaccountrepository;
+	
 	@RequestMapping(value = "qaaccounts", method = RequestMethod.GET)
 	public List<QAAccount> list(){
-		return QAAccountStub.list();
+		return qaaccountrepository.findAll();
 	}
 	
 	@RequestMapping(value = "qaaccounts", method = RequestMethod.POST)
 	public QAAccount create(@RequestBody QAAccount qaaccount) {
-		return QAAccountStub.create(qaaccount);
+		return qaaccountrepository.saveAndFlush(qaaccount);
 	}
 	
 	@RequestMapping(value = "qaaccounts/{id}", method = RequestMethod.GET)
 	public QAAccount get(@PathVariable Long id) {
-		return QAAccountStub.get(id);
+		return qaaccountrepository.findOne(id);
 	}
 	
 	@RequestMapping(value = "qaaccounts/{id}", method = RequestMethod.PUT)
 		public QAAccount update(@PathVariable Long id, @RequestBody QAAccount qaaccount) {
-		return QAAccountStub.update(id, qaaccount);
+		QAAccount existingQAAccount = qaaccountrepository.findOne(id);
+		BeanUtils.copyProperties(qaaccount, existingQAAccount);
+		return qaaccountrepository.saveAndFlush(existingQAAccount);
 	}
 	
 	@RequestMapping(value = "qaaccounts/{id}", method = RequestMethod.DELETE)
 	public QAAccount delete(@PathVariable Long id) {
-		return QAAccountStub.delete(id);
+		QAAccount existingQAAccount = qaaccountrepository.findOne(id);
+		qaaccountrepository.delete(existingQAAccount);
+		return existingQAAccount;
 	}
 }
 
