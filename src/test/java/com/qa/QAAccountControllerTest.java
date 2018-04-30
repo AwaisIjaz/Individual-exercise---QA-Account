f69301b7bfbaa0669418b8132058c767630c63ff
AwaisIjaz <Awais_ijaz786@hotmail.com>
2018-04-30 08:50:43 +0100

    qaaccount controller test passed using mockito

--- src/test/java/com/qa/QAAccountControllerTest.java
+++ src/test/java/com/qa/QAAccountControllerTest.java
@@ -2,19 +2,37 @@ package com.qa;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
 
 import com.qa.controller.QAAccountController;
 import com.qa.model.QAAccount;
+import com.qa.repository.QAAccountRepository;
 
 public class QAAccountControllerTest {
+	@InjectMocks
+	private QAAccountController qc;
+	
+	@Mock
+	private QAAccountRepository qaaccountrepository;
+	
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
 	@Test
 	public void testQAAccountGet() {
-		QAAccountController qc = new QAAccountController();
-		QAAccount account = qc.get(1L);
-		assertEquals(1L, account.getId().longValue());
+		QAAccount qa = new QAAccount();
+		qa.setId(1l);
+		when(qaaccountrepository.findOne(1l)).thenReturn(qa);
 		
-		
-		
+		QAAccount account = qc.get(1l);
+		assertEquals(1l, account.getId().longValue());
+
 	}
 }
