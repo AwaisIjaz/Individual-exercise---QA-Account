c20d6ec8b24a32d2c93fcafebd44270664d67665
AwaisIjaz <Awais_ijaz786@hotmail.com>
2018-04-30 11:45:52 +0100

    Front end fixed

--- src/main/resources/public/js/controllers.js
+++ src/main/resources/public/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('app.controllers', []).controller('QAAccountListController', function($scope, $state, popupService, $window, QAAccount) {
-  $scope.accounts = QAAccount.query(); //fetch all shipwrecks. Issues a GET to /api/vi/shipwrecks
+  $scope.qaaccounts = QAAccount.query(); 
 
-  $scope.deleteQAAccount = function(qaaccount) { // Delete a Shipwreck. Issues a DELETE to /api/v1/shipwrecks/:id
+  $scope.deleteQAAccount = function(qaaccount) { 
     if (popupService.showPopup('Really delete this?')) {
       qaaccount.$delete(function() {
         $scope.qaaccounts = QAAccount.query(); 
@@ -10,25 +10,25 @@ angular.module('app.controllers', []).controller('QAAccountListController', func
     }
   };
 }).controller('QAAccountViewController', function($scope, $stateParams, QAAccount) {
-  $scope.qaaccount = QAAccount.get({ id: $stateParams.id }); //Get a single shipwreck.Issues a GET to /api/v1/shipwrecks/:id
+  $scope.qaaccount = QAAccount.get({ id: $stateParams.id }); 
 }).controller('QAAccountCreateController', function($scope, $state, $stateParams, QAAccount) {
-  $scope.qaaccount = new QAAccount();  //create new shipwreck instance. Properties will be set via ng-model on UI
+  $scope.qaaccount = new QAAccount();  
 
-  $scope.addQAAccount = function() { //create a new shipwreck. Issues a POST to /api/v1/shipwrecks
+  $scope.addQAAccount = function() { 
     $scope.qaaccount.$save(function() {
-      $state.go('qaaccounts'); // on success go back to the list i.e. shipwrecks state.
+      $state.go('qaaccounts'); 
     });
   };
 }).controller('QAAccountEditController', function($scope, $state, $stateParams, QAAccount) {
-  $scope.updateQAAccount = function() { //Update the edited shipwreck. Issues a PUT to /api/v1/shipwrecks/:id
+  $scope.updateQAAccount = function() { 
     $scope.qaaccount.$update(function() {
-      $state.go('qaaccounts'); // on success go back to the list i.e. shipwrecks state.
+      $state.go('qaaccounts'); 
     });
   };
 
-  $scope.loadQAAccount = function() { //Issues a GET request to /api/v1/shipwrecks/:id to get a shipwreck to update
+  $scope.loadQAAccount = function() { 
     $scope.qaaccount = QAAccount.get({ id: $stateParams.id });
   };
 
-  $scope.loadQAAccount(); // Load a shipwreck which can be edited on UI
+  $scope.loadQAAccount(); 
 });
