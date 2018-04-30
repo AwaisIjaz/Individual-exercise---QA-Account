e09f16dbf1f0d5caeda4d742a26567670f8a21a2
AwaisIjaz <Awais_ijaz786@hotmail.com>
2018-04-30 08:32:33 +0100

    testing framework for homecontroller created

--- src/test/java/com/qa/AppTest.java
+++ src/test/java/com/qa/AppTest.java
@@ -1,38 +1,19 @@
 package com.qa;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Test;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import com.qa.controller.HomeController;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+
+public class AppTest {
+    
+	@Test
+    public void testApp(){
+		HomeController hc = new HomeController();
+		String result = hc.home();
+		assertEquals();
+	}		 
 }
